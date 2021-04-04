958db8918c52d87ebc1b7713b3ddd29fd77287e5
Marcus Loo <marcuslooek@gmail.com>
2021-04-04 11:29:56 +0800

    Added new pages

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -7,15 +7,33 @@ import '../controllers/home_controller.dart';
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Geofence Simulator'),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
+              Tab(
+                icon: Icon(Icons.location_on),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text('Settings goes here'),
+            ),
+            Center(
+              child: Text('checking of geofence goes here'),
+            ),
+          ],
         ),
       ),
     );
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,6 +2,8 @@ import 'package:get/get.dart';
 
 import 'package:geofence/app/modules/home/bindings/home_binding.dart';
 import 'package:geofence/app/modules/home/views/home_view.dart';
+import 'package:geofence/app/modules/settings/bindings/settings_binding.dart';
+import 'package:geofence/app/modules/settings/views/settings_view.dart';
 
 part 'app_routes.dart';
 
@@ -13,6 +15,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SETTINGS,
+      page: () => SettingsView(),
+      binding: SettingsBinding(),
     ),
   ];
 }
